67b73306b10acb34f4fb0faedd147ecf0e12a323
dabidstudio <152172802+dabidstudio@users.noreply.github.com>
2025-07-13 22:11:34 +0900

    Update 1_basic.py

--- 1_basic.py
+++ 1_basic.py
@@ -1,14 +1,17 @@
 import asyncio
 from agents import Agent, Runner
+from dotenv import load_dotenv
+load_dotenv()
 
 async def main():
    agent = Agent(
        name="여행 에이전트",
-       instructions="당신은 훌륭한 여행 에이전트입니다."
+       instructions="당신은 훌륭한 여행 에이전트입니다.",
+       model="gpt-4.1"
    )
    prompt = "강원도 여행지 추천해줘"
    result = await Runner.run(agent, prompt)
-   print(result.final_output)
-
+   print(result.final_output)    
+   
 if __name__ == "__main__":
    asyncio.run(main())  
